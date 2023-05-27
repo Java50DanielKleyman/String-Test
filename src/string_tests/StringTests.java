9e2d107f85ac91fcdb069f95d54d476ec2a7a44b
Daniel Kleyman <denny_klm1@yahoo.com>
2023-05-27 15:39:17 +0300

    HW-1

--- src/string_tests/StringTests.java
+++ src/string_tests/StringTests.java
@@ -7,8 +7,46 @@ import org.junit.jupiter.api.Test;
 class StringTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void compareTotest() {
+		String str = "Hello";
+		assertEquals(0, str.compareTo("Hello"));
 	}
-
+	@Test
+	void compareToIgnoreCasetest() {
+		String str = "Hello";
+		assertEquals(0, str.compareToIgnoreCase("Hello"));
+	}
+	@Test
+	void concattest() {
+		String str = "Hello";
+		assertEquals("HelloWorld!!!", str.concat("World!!!"));
+	}
+	@Test
+	void startsWithtest() {
+		String str = "Hello";
+		assertTrue(str.startsWith("He"));
+		assertFalse(str.startsWith("Hew"));
+	}
+	@Test
+	void endsWithtest() {
+		String str = "Hello";
+		assertTrue(str.endsWith("llo"));
+		assertFalse(str.endsWith("Hew"));
+	}
+	@Test
+	void equalsIgnoreCasetest() {
+		String str = "Hello";
+		assertTrue(str.equalsIgnoreCase("Hello"));
+		assertFalse(str.equalsIgnoreCase("Hew"));
+	}
+	@Test
+	void indexOftest() {
+		String str = "Hello";
+		assertEquals(4, str.indexOf("o"));		
+	}
+	@Test
+	void lastIndexOftest() {
+		String str = "Hello world";
+		assertEquals(9, str.lastIndexOf('l'));		
+	}
 }
