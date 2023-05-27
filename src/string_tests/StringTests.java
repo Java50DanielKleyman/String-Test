40305a58bdc37b046d0f086581495030c0c1df08
Daniel Kleyman <denny_klm1@yahoo.com>
2023-05-27 14:55:38 +0300

    HW-1

---
+++ src/string_tests/StringTests.java
@@ -0,0 +1,14 @@
+package string_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
